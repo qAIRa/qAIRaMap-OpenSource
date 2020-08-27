563fbe464d9d464d1e93aa3065d821668bdafde3
SabCampos <s.campos@qairadrones.com>
2020-08-26 21:46:35 -0500

    babel config

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        // {
+        //     "useBuiltIns": "entry"
+        //   },
+        {
+          targets: {
+            node: 'current',
+            "chrome": "65",
+            "safari": "12",
+            "firefox": "60",
+            "opera": "60",
+            "android": "7.0",
+            "ios": "12.4",
+          },
+        },
+      ],
+    ],
+  };
