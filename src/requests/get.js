532138b74d5478e83986b084c92e778b3f0c3cfc
SabCampos <s.campos@qairadrones.com>
2020-09-01 19:02:23 -0500

    unhandled-rejections

--- src/requests/get.js
+++ src/requests/get.js
@@ -1,23 +1,30 @@
 import {APISource} from '../index.js';
+const handleError =  (err)=> {
+    console.throw(err);
+    return new Response(JSON.stringify({
+        code: 400,
+        message: 'Stupid network Error xD'
+    }));
+};
 
 const requestAllQhawax = async() => {
-    const response = await fetch(`${APISource}AllQhawaxInMap/`);
+    const response = await fetch(`${APISource}AllQhawaxInMap/`).catch(err => handleError(err));
     return  await response.json();
 }
 
 const requestAverageMeasurement = async (qhawax, sensor) => {
-    const response = await fetch(`https://qairamapnapi-dev-opensource.qairadrones.com/api/gas_average_measurement/?qhawax=${qhawax}&gas=${sensor}`);
+    const response = await fetch(`https://qairamapnapi-dev-opensource.qairadrones.com/api/gas_average_measurement/?qhawax=${qhawax}&gas=${sensor}`).catch(err => handleError(err));
     return  await response.json();
 }
 
 
 const requestBinnacle = async (ID) => {
-    const response = await fetch(`https://qairamapnapi-dev-opensource.qairadrones.com/api/get_all_observations_by_qhawax/?qhawax_id=${ID}`);
+    const response = await fetch(`https://qairamapnapi-dev-opensource.qairadrones.com/api/get_all_observations_by_qhawax/?qhawax_id=${ID}`).catch(err => handleError(err));
     return await response.json();
 };
 
 const requestStatus = async (ID) =>{
-    const response = await fetch(`https://qairamapnapi-dev-opensource.qairadrones.com/api/qhawax_status/?name=${ID}`);
+    const response = await fetch(`https://qairamapnapi-dev-opensource.qairadrones.com/api/qhawax_status/?name=${ID}`).catch(err => console.log(err));
     return await response.text()
 };
 const downloadData = async(check, id,init, end) =>{
@@ -25,11 +32,11 @@ const downloadData = async(check, id,init, end) =>{
 				? `${APISource}valid_processed_measurements_period/?qhawax_id=${id}&initial_timestamp=${init}&final_timestamp=${end}`
 				: `${APISource}average_valid_processed_period/?qhawax_id=${id}&initial_timestamp=${init}&final_timestamp=${end}`;
 
-    const response = await fetch(URL);
+    const response = await fetch(URL).catch(err => handleError(err));
     return await response.json();
 };
 const requestInstallationDate = async (ID)=>{
-    const response = await fetch(`${APISource}GetInstallationDate/?qhawax_id=${ID}`);
+    const response = await fetch(`${APISource}GetInstallationDate/?qhawax_id=${ID}`).catch(err => console.log(err));
     return await response.text()
 };
 export { 
--- test/get.spec.js
+++ test/get.spec.js
@@ -9,9 +9,6 @@ import {
 import { enableFetchMocks } from 'jest-fetch-mock';
 enableFetchMocks()
 
-beforeEach(() => { 
-  fetchMock.doMock()
-})
 
   describe('testing api', () => {
     beforeEach(() => {
@@ -19,11 +16,11 @@ beforeEach(() => {
     })
    
     it('calls installation date', () => {
-      fetch.mockResponseOnce(JSON.stringify({ data: '12345' }))
+      fetch.mockResponseOnce('calls installation date')
    
       //assert on the response
       requestInstallationDate(4).then(res => {
-        expect(res.data).toEqual('12345')
+        expect(res).toEqual('calls installation date')
       })
    
       //assert on the times called and arguments given to fetch
@@ -32,11 +29,11 @@ beforeEach(() => {
     })
 
     it('calls all qHAWAXs', () => {
-        fetch.mockResponseOnce(JSON.stringify({ data: '12345' }))
+        fetch.mockResponseOnce(JSON.stringify({ data: 'calls all qHAWAXs' }))
      
         //assert on the response
         requestAllQhawax().then(res => {
-          expect(res.data).toEqual('12345')
+          expect(res.data).toEqual('calls all qHAWAXs')
         })
      
         //assert on the times called and arguments given to fetch
@@ -45,11 +42,11 @@ beforeEach(() => {
       });
 
       it('calls binnacle', () => {
-        fetch.mockResponseOnce(JSON.stringify({ data: '12345'}))
+        fetch.mockResponseOnce(JSON.stringify({ data: 'calls binnacle'}))
      
         //assert on the response
         requestBinnacle(4).then(res => {
-          expect(res.data).toEqual('12345')
+          expect(res.data).toEqual('calls binnacle')
         })
      
         //assert on the times called and arguments given to fetch
@@ -57,39 +54,36 @@ beforeEach(() => {
         expect(fetch.mock.calls[0][0]).toEqual('https://qairamapnapi-dev-opensource.qairadrones.com/api/get_all_observations_by_qhawax/?qhawax_id=4')
       });
       it('calls status', () => {
-        fetch.mockResponseOnce(JSON.stringify({ data: '12345'}))
+        fetch.mockResponseOnce('calls status');
      
         //assert on the response
         requestStatus(4).then(res => {
-          expect(res.data).toEqual('12345')
+          expect(res).toEqual('calls status')
         })
-     
         //assert on the times called and arguments given to fetch
         expect(fetch.mock.calls.length).toEqual(1)
         expect(fetch.mock.calls[0][0]).toEqual('https://qairamapnapi-dev-opensource.qairadrones.com/api/qhawax_status/?name=4')
       })
       it('calls download data', () => {
-        fetch.mockResponseOnce(JSON.stringify({ data: '12345'}))
+        fetch.mockResponseOnce(JSON.stringify({ data: 'calls download data'}))
         //assert on the response
         downloadData(true, 4,'01-09-2020 05:00:00', '01-09-2020 18:00:00').then(res => {
-          expect(res.data).toEqual('12345')
+          expect(res.data).toEqual('calls download data')
         })
         downloadData(false, 4,'01-09-2020 05:00:00', '01-09-2020 18:00:00').then(res => {
-            expect(res.data).toEqual('12345')
+            expect(res.data).toEqual('calls download data')
           })
-     
         //assert on the times called and arguments given to fetch
         expect(fetch.mock.calls.length).toEqual(2)
         expect(fetch.mock.calls[0][0]).toEqual('https://qairamapnapi-dev-opensource.qairadrones.com/api/valid_processed_measurements_period/?qhawax_id=4&initial_timestamp=01-09-2020 05:00:00&final_timestamp=01-09-2020 18:00:00')
         expect(fetch.mock.calls[1][0]).toEqual('https://qairamapnapi-dev-opensource.qairadrones.com/api/average_valid_processed_period/?qhawax_id=4&initial_timestamp=01-09-2020 05:00:00&final_timestamp=01-09-2020 18:00:00')
       });
       it('calls average measurements', () => {
-        fetch.mockResponseOnce(JSON.stringify({ data: '12345'}))
+        fetch.mockResponseOnce(JSON.stringify({ data: 'calls average measurements'}))
         //assert on the response
         requestAverageMeasurement(4,'CO').then(res => {
-          expect(res.data).toEqual('12345')
+          expect(res.data).toEqual('calls average measurements')
         })
-     
         //assert on the times called and arguments given to fetch
         
         expect(fetch.mock.calls.length).toEqual(1)
