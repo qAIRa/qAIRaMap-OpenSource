c34c65ed77929706be78ae1591c9d1dc5826f0dc
SabCampos <s.campos@qairadrones.com>
2020-09-03 04:46:57 -0500

    ordering HtmlComponents

--- src/lib/HtmlComponents.js
+++ src/lib/HtmlComponents.js
@@ -5,33 +5,20 @@ const viewMap = `
 <div class="animate__animated animate__swing" id="over_map">
 <h6><strong>Air Quality Index (INCA)</strong></h6>
     <table class="centered">
-        <thead>
-          <tr>
-              <th>Good</th>
-              <th>Moderate</th>
-			        <th>Bad</th>
-			        <th>Hazardous</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td bgcolor="#009966" class="inca-color"></td>
-            <td bgcolor="#ffde33" class="inca-color"></td>
+        <thead><tr><th>Good</th><th>Moderate</th><th>Bad</th><th>Hazardous</th></tr></thead>
+        <tbody><tr><td bgcolor="#009966" class="inca-color"></td>
+                <td bgcolor="#ffde33" class="inca-color"></td>
 	      		<td bgcolor="#ff9933" class="inca-color"></td>
-			      <td bgcolor="#cc0033" class="inca-color"></td>
-		      </tr>
-		      <tr>
-            <td><img class="inca-qairito" id="qairito-good" src="img/qairito/qairito_buena.gif" alt="qairito-good"></img></td>
+			    <td bgcolor="#cc0033" class="inca-color"></td></tr>
+		  <tr><td><img class="inca-qairito" id="qairito-good" src="img/qairito/qairito_buena.gif" alt="qairito-good"></img></td>
             <td><img class="inca-qairito" id="qairito-moderate" src="img/qairito/qairito_moderada.gif" alt="qairito-moderate" ></img></td>
-			      <td><img class="inca-qairito" id="qairito-bad" src="img/qairito/qairito_mala.gif" alt="qairito-bad" ></img></td>
-			      <td><img class="inca-qairito" id="qairito-hazardous" src="img/qairito/qairito_cuidado.gif" alt="qairito-hazardous"></img></td>
+			<td><img class="inca-qairito" id="qairito-bad" src="img/qairito/qairito_mala.gif" alt="qairito-bad" ></img></td>
+			<td><img class="inca-qairito" id="qairito-hazardous" src="img/qairito/qairito_cuidado.gif" alt="qairito-hazardous"></img></td>
           </tr>
         </tbody>
   </table>
 </div>
 </div>
-
-
 <!-- Modal Graphics -->
 <div id="modalGraphic" class="modal">
   <a id="close" class="modal-close right responsive-img modal-images-close">X</a>
@@ -42,33 +29,20 @@ const viewMap = `
 
 const navbar =(structure) =>`
 <div class="navbar-fixed">
-
 <nav id="nav-menu-bar" class="transparent" style="padding: 0px 10px;">
 <div id="nav-wrapper-menu-bar" class="nav-wrapper">
-
     <ul id="menu-left-bar" class="left hide-on-med-and-down">
     <li id="return-menu" type="button"><a >Home</a></li>
     </ul> 
-
     <a href="https://www.qairadrones.com" class="brand-logo center"id="brand-logo-menu-bar">
-        <img
-        
-            src="/img/logo-white.png"
-            alt="logo qAIRa"
-            id="logo-menu-qAIRa"
-            style="max-width: 4.5em; max-height: 2em"
-
-        />
+        <img src="/img/logo-white.png" alt="logo qAIRa"id="logo-menu-qAIRa"style="max-width: 4.5em; max-height: 2em"/>
     </a>
     <ul id="menu-list-bar" class="right hide-on-med-and-down">
-
     <li id="legend-menu"><a class="dropdown-trigger" href="#" data-target="dropdown1">Legend</a></li>
     <li id="download-menu"><a>Download</a></li>
     <li id="dashboard-menu"><a>Dashboard</a></li>
     <li id="graphics-menu"><a>Graphics</a></li>
-
     </ul>
-
     <a href="#" id="menu-trigger" class="sidenav-trigger" data-target="mobile-nav" ><i class="material-icons">menu</i></a>
 </div>
 </nav>
@@ -86,85 +60,46 @@ ${structure}
 `;
 
 const dropdown = `
-
 <!-- Modal Air INCA Structure -->
         <div id="modalAirInca" class="modal">
             <span class="modal-close right">X</span>
             <div class="modal-content center">
-                <p>
-                    <a
-                        href="http://www.minam.gob.pe/wp-content/uploads/2016/07/RM-N%C2%B0-181-2016-MINAM.pdf"
-                    >
-                        Source: Resolución Ministerial N°-181-2016-MINAM.
-                    </a>
-                </p>
-                <img
-                    class="responsive-img"
-                    src="/img/INCA - Calidad del Aire.png"
-                    alt="Tabla colores INCA"
-                />
+                <p><a href="http://www.minam.gob.pe/wp-content/uploads/2016/07/RM-N%C2%B0-181-2016-MINAM.pdf"> Source: Resolución Ministerial N°-181-2016-MINAM.</a>
+                </p><img class="responsive-img" src="/img/INCA - Calidad del Aire.png" alt="Tabla colores INCA" />
             </div>
             <div class="modal-footer"></div>
         </div>
-
-        <!-- Modal Ruido ECA Structure -->
+ <!-- Modal Ruido ECA Structure -->
         <div id="modalRuidoEca" class="modal">
             <span class="modal-close right">X</span>
             <div class="modal-content center">
-                <p>
-                    <a href="https://www.oefa.gob.pe/?wpfb_dl=19087">
-                        Source: Contaminación Sonora OEFA
-                    </a>
-                </p>
-                <img
-                    class="responsive-img"
-                    src="/img/ECA-Ruido.png"
-                    alt="Tabla colores ECA-Ruido"
-                />
+                <p> <a href="https://www.oefa.gob.pe/?wpfb_dl=19087">  Source: Contaminación Sonora OEFA </a> </p>
+                <img class="responsive-img" src="/img/ECA-Ruido.png" alt="Tabla colores ECA-Ruido"/>
             </div>
             <div class="modal-footer"></div>
         </div>
-
-        <!-- Modal UV ECA Structure -->
+ <!-- Modal UV ECA Structure -->
         <div id="modalUVEca" class="modal">
             <span class="modal-close right">X</span>
             <div class="modal-content center">
-                <p>
-                    <a
-                        href="https://apps.who.int/iris/bitstream/handle/10665/42633/9243590073.pdf;jsessionid=A51BDAB9660F30189903E31DAAF806ED?sequence=1		"
-                    >
-                        Source: OMS-Radiación Ultravioleta-2003
-                    </a>
+                <p><a href="https://apps.who.int/iris/bitstream/handle/10665/42633/9243590073.pdf;jsessionid=A51BDAB9660F30189903E31DAAF806ED?sequence=1" > Source: OMS-Radiación Ultravioleta-2003</a>
                 </p>
-                <img
-                    class="responsive-img"
-                    src="/img/Indice-UV.png"
-                    alt="Tabla colores UV"
-                />
+                <img class="responsive-img" src="/img/Indice-UV.png" alt="Tabla colores UV"/>
             </div>
             <div class="modal-footer"></div>
         </div>
 
-        <!-- Modal Aire ECA Structure -->
+<!-- Modal Aire ECA Structure -->
         <div id="modalAireEca" class="modal">
             <span class="modal-close right">X</span>
             <div class="modal-content center">
-                <p>
-                    <a
-                        href="http://www.minam.gob.pe/wp-content/uploads/2017/06/DS-003-2017-MINAM.pdf"
-                    >
-                        Source: DS Nº 003-2017-MINAM
-                    </a>
+                <p><a href="http://www.minam.gob.pe/wp-content/uploads/2017/06/DS-003-2017-MINAM.pdf"> Source: DS Nº 003-2017-MINAM </a>
                 </p>
-                <img
-                    class="responsive-img"
-                    src="/img/ECA-aire.png"
-                    alt="Tabla ECA Aire"
-                />
+                <img class="responsive-img" src="/img/ECA-aire.png" alt="Tabla ECA Aire"/>
             </div>
             <div class="modal-footer"></div>
         </div>
-        <!-- Dropdown Structure 1-->
+ <!-- Dropdown Structure 1-->
         <ul id="dropdown1" class="dropdown-content">
             <li><a class="modal-trigger" href="#modalAirInca">Gas Legend (INCA)</a></li>
             <li class="divider" tabindex="-1"></li>
@@ -174,7 +109,7 @@ const dropdown = `
             <li class="divider" tabindex="-1"></li>
             <li><a class="modal-trigger" href="#modalAireEca">Air Legend (ECA)</a> </li>
         </ul>
-        <!-- Dropdown Structure 2-->
+<!-- Dropdown Structure 2-->
         <ul id="dropdown2" class="dropdown-content">
             <li><a class="modal-trigger" href="#modalAirInca">Gas Legend (INCA)</a></li>
             <li class="divider" tabindex="-1"></li>
@@ -195,11 +130,8 @@ const viewDownload = `
                 <div class="row">
                     <div class="input-field col s6 offset-s3">
                               <div class="switch center-align">
-                              <label>
-                              Hourly average
-                                <input type="checkbox" checked="checked" id="select-data">
-                                <span class="lever"></span>
-                              Raw Data
+                              <label> Hourly average <input type="checkbox" checked="checked" id="select-data"> <span class="lever">
+                              </span> Raw Data
                               </label>
                               </div><br>
                         <select class="browser-default center-align" name="" id="selectQhawax">
@@ -242,10 +174,8 @@ const viewDownload = `
       The data has not passed a quality control.</sub></strong></p>
         </div>
     </div>
-	</div>
-	
+	</div>	
 `;
-
 
     const viewBoard = `   
 <table class="responsive-table highlight centered table-calibration">
@@ -326,8 +256,6 @@ const chartView = `
     </div>
         
 `;
-
-
 
 export { viewMap,
   navbar,
--- src/views/graphicsView.js
+++ src/views/graphicsView.js
@@ -31,29 +31,46 @@ const requestOptions = async (element) => {
 		status==='ON'?addOptions.appendChild(option):false;
 	});
  };
+ let yAxis = {
+	temperature : {value:[],name: 'Temperatura (C)'},
+	pressure : {value:[],name: 'Presión (Pa)'},
+	humidity : {value:[],name: 'Humedad (%)'},
+	 CO : {value:[],name: 'CO (ppb)'},
+	 H2S : {value:[],name: 'H2S (ppb)'},
+	 NO2 : {value:[],name: 'NO2 (ppb)'},
+	 O3 : {value:[],name: 'O3 (ppb)'},
+	 SO2 : {value:[],name: 'SO2 (ppb)'},
+	 PM1 : {value:[],name: 'PM1 (ug/m3)'},
+	 PM10 : {value:[],name: 'PM10 (ug/m3)'},
+	 PM25 : {value:[],name: 'PM2,5 (ug/m3)'},
+	 spl : {value:[],name: 'Ruido (dB)'},
+	 UV : {value:[],name: 'UV'},
+	 UVA : {value:[],name: 'UVA (mW/m2)'},
+	 UVB : {value:[],name: 'UVA (mW/m2)'},	 
+	 
+};
+let sensors=['temperature',
+	'pressure',
+	'humidity',
+	'CO',
+	'H2S',
+	'NO2',
+	'O3',
+	'SO2',
+	'PM1',
+	'PM10',
+	'PM25',
+	'spl',
+	'UV',
+	'UVA',
+	'UVB'
+];
 
  const createTraces = async (time, qhawax, charts) => {
 	let traces = [];
 
 	const json = await requestGraphicsData(qhawax, time)
-	let yAxis = {
-		temperature : {value:[],name: 'Temperatura (C)'},
-		pressure : {value:[],name: 'Presión (Pa)'},
-		humidity : {value:[],name: 'Humedad (%)'},
-		 CO : {value:[],name: 'CO (ppb)'},
-		 H2S : {value:[],name: 'H2S (ppb)'},
-		 NO2 : {value:[],name: 'NO2 (ppb)'},
-		 O3 : {value:[],name: 'O3 (ppb)'},
-		 SO2 : {value:[],name: 'SO2 (ppb)'},
-		 PM1 : {value:[],name: 'PM1 (ug/m3)'},
-		 PM10 : {value:[],name: 'PM10 (ug/m3)'},
-		 PM25 : {value:[],name: 'PM2,5 (ug/m3)'},
-		 spl : {value:[],name: 'Ruido (dB)'},
-		 UV : {value:[],name: 'UV'},
-		 UVA : {value:[],name: 'UVA (mW/m2)'},
-		 UVB : {value:[],name: 'UVA (mW/m2)'},	 
-		 
-	}
+
 	 let y = [];
 	 let x = [];
 	json.forEach(d => {
@@ -88,16 +105,12 @@ const viewGraphics = () => {
 	const graphElem = document.createElement('div');
 	graphElem.setAttribute('class', 'container');
 	navBarClient(graphElem, chartView, )
-	
-
 	const graphBtn = graphElem.querySelector('#graphicBtn');
 
 	const selection = graphElem.querySelectorAll('select');
 	M.FormSelect.init(selection);
-
 	 requestOptions(graphElem );
 	const charts = graphElem.querySelectorAll('.chart');
-
 	let selectedQhawax = '';
 	selection[0].onchange = () => {
 		selectedQhawax = selection[0].value.toString();
@@ -107,27 +120,9 @@ const viewGraphics = () => {
 		selectedTime = selection[1].value;
 	};
 	
-
 	graphBtn.addEventListener('click', () => {
 		createTraces(selectedTime, selectedQhawax, charts);
-
 		const socket = io.connect(`${SocketSource}`);
-		let sensors=['temperature',
-			'pressure',
-			'humidity',
-			'CO',
-			'H2S',
-			'NO2',
-			'O3',
-			'SO2',
-			'PM1',
-			'PM10',
-			'PM25',
-			'spl',
-			'UV',
-			'UVA',
-			'UVB']
-
 		socket.on('new_data_summary_processed', res => {
 			if (res.ID === selectedQhawax) {
 				let index=0;
