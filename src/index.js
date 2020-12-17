d950d0e5738d98b0449f569adb567d316fdb597a
SabCampos <s.campos@qairadrones.com>
2020-12-17 11:45:58 -0500

    fix download

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ const SocketSource = 'https://qairamapnapi-dev-opensource.qairadrones.com/';
 
 window.onload = () => {
   document.getElementById('loader').classList.add('hide');
-
-  const chipAlert = document.querySelectorAll('.chip');
-  M.Chips.getInstance(chipAlert);
-
   window.onhashchange = changeView(window.location.hash);
 };
 
