9cb6d5adacc254fc2c2df06b1f658fba17886f50
SabCampos <s.campos@qairadrones.com>
2021-06-04 09:49:14 -0500

    -VOC, -CO2, +H, +T infowin

--- src/html/freeMapMobileQ.js
+++ src/html/freeMapMobileQ.js
@@ -26,14 +26,12 @@ export const mobileSelection =`
 <select class="browser-default col s6" name="" id="selectSensor">
 <option value="" disabled selected>Sensor</option>
 <option value="CO">CO<sub>(ug/m3)</sub></option>
-<option value="CO2">CO<sub>2</sub><sub>(ppm)</sub></option>
 <option value="NO2">NO<sub>2</sub><sub>(ug/m3)</sub></option>
 <option value="O3">O<sub>3</sub><sub>(ug/m3)</sub></option>
 <option value="H2S">H<sub>2</sub>S<sub>(ug/m3)</sub></option>
 <option value="SO2">SO<sub>2</sub><sub>(ug/m3)</sub></option>
 <option value="PM25">PM<sub>2,5</sub><sub>(ug/m3)</sub></option>
 <option value="PM10">PM<sub>10</sub><sub>(ug/m3)</sub></option>
-<option value="VOC">VOC<sub>(ug/m3)</sub></option>
 </select>
 <button id="draw-btn" class="btn waves-effect waves-light col s4 offset-s4 disabled" >Dibujar
 <i class="material-icons right">send</i>
--- src/lib/infowindow.js
+++ src/lib/infowindow.js
@@ -32,49 +32,15 @@ const notNull = (value) => {
   return value===null||value<0?'-':value;
 }
 
-// export const infoWindowM = (data, q_mobile, timer)=>`
-// <table class="tg">
-// <caption>${q_mobile.name}: ${q_mobile.comercial_name}</caption>
-// <thead>
-//   <tr>
-//     <th class="tg-wa1i">CO<br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">CO<sub>2</sub><br>(ppm</sup>)</th>
-//     <th class="tg-wa1i">NO<sub>2</sub><br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">O<sub>3</sub><br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">H<sub>2</sub>S<br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">SO<sub>2</sub><br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">PM<sub>2,5</sub><br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">PM<sub>10</sub><br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">VOC<br>(&micro;g/m<sup>3</sup>)</th>
-//     <th class="tg-wa1i">Time min</th>
-//   </tr>
-// </thead>
-// <tbody>
-//   <tr>
-//     <td class="tg-6k50">${notNull(data.CO_ug_m3)}</td>
-//     <td class="tg-6k50">${notNull(data.CO2)}</td>
-//     <td class="tg-6k50">${notNull(data.NO2_ug_m3)}</td>
-//     <td class="tg-6k50">${notNull(data.O3_ug_m3)}</td>
-//     <td class="tg-6k50">${notNull(data.H2S_ug_m3)}</td>
-//     <td class="tg-6k50">${notNull(data.SO2_ug_m3)}</td>
-//     <td class="tg-6k50">${notNull(data.PM25)}</td>
-//     <td class="tg-6k50">${notNull(data.PM10)}</td>
-//     <td class="tg-6k50">${notNull(data.VOC)}</td>
-//     <td class="tg-6k50">${timer.minutes}:${timer.seconds}</td>
-//   </tr>
-// </tbody>
-// </table>
-// `;
-
-
-
+// style="border:1px double ${ cellColor('CO',data.CO_ug_m3)};
+// bgcolor=${ cellColor('SO2',data.SO2_ug_m3)}
 export const infoWindowM = (data, q_mobile, timer)=>`
 <table class="tg stripped centered info-mobile">
 <caption>${q_mobile.name}: ${q_mobile.comercial_name}</caption>
 <thead>
   <tr>
     <th class="tg-wa1i">CO<br>(&micro;g/m<sup>3</sup>)</th>
-    <th class="tg-wa1i">CO<sub>2</sub><br>(ppm</sup>)</th>
+    <th class="tg-wa1i">SO<sub>2</sub><br>(&micro;g/m<sup>3</sup>)</th>
     <th class="tg-wa1i">NO<sub>2</sub><br>(&micro;g/m<sup>3</sup>)</th>
     <th class="tg-wa1i">O<sub>3</sub><br>(&micro;g/m<sup>3</sup>)</th>
     <th class="tg-wa1i">H<sub>2</sub>S<br>(&micro;g/m<sup>3</sup>)</th>
@@ -83,7 +49,7 @@ export const infoWindowM = (data, q_mobile, timer)=>`
 <tbody>
   <tr>
     <td class="tg-6k50">${notNull(data.CO_ug_m3)}</td>
-    <td class="tg-6k50">${notNull(data.CO2)}</td>
+    <td class="tg-6k50">${notNull(data.SO2_ug_m3)}</td>
     <td class="tg-6k50">${notNull(data.NO2_ug_m3)}</td>
     <td class="tg-6k50">${notNull(data.O3_ug_m3)}</td>
     <td class="tg-6k50">${notNull(data.H2S_ug_m3)}</td>
@@ -94,20 +60,20 @@ export const infoWindowM = (data, q_mobile, timer)=>`
 <table class="tg stripped centered info-mobile">
 <thead>
   <tr>
-    <th class="tg-wa1i">SO<sub>2</sub><br>(&micro;g/m<sup>3</sup>)</th>
     <th class="tg-wa1i">PM<sub>2,5</sub><br>(&micro;g/m<sup>3</sup>)</th>
     <th class="tg-wa1i">PM<sub>10</sub><br>(&micro;g/m<sup>3</sup>)</th>
-    <th class="tg-wa1i">VOC<br>(&micro;g/m<sup>3</sup>)</th>
-    <th class="tg-wa1i">Time min</th>
+    <th class="tg-wa1i">T<br>(°C)</th>
+    <th class="tg-wa1i">H<br>(%)</th>
+    <th class="tg-wa1i">Tiempo (min)</th>
   </tr>
 </thead>
 <tbody>
   <tr>
-    <td class="tg-6k50">${notNull(data.SO2_ug_m3)}</td>
     <td class="tg-6k50">${notNull(data.PM25)}</td>
     <td class="tg-6k50">${notNull(data.PM10)}</td>
-    <td class="tg-6k50">${notNull(data.VOC)}</td>
-    <td class="tg-6k50">${timer.minutes}:${timer.seconds}</td>
+    <td class="tg-6k50">${notNull(data.temperature)}</td>
+    <td class="tg-6k50">${notNull(data.humidity)}</td>
+    <td class="tg-6k50">${timer.hours}:${timer.minutes}:${timer.seconds}</td>
   </tr>
 </tbody>
 </table>
