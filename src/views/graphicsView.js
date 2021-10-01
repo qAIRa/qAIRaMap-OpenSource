13d7a3a1b7c26809db9257ff90976ca65bfbd34e
nmagliui <nataly.magluy@gmail.com>
2021-10-01 11:50:10 -0700

    remove local prettier changes

--- src/views/graphicsView.js
+++ src/views/graphicsView.js
@@ -1,13 +1,9 @@
-import { navBarQhawax } from "../lib/navBarQhawax.js";
-import { chartView } from "../html/graphics.js";
-import {
-  oneParameterRequest,
-  requestGraphicsData,
-  noParametersRequest,
-} from "../requests/get.js";
-import { socket } from "../index.js";
-import { addZero } from "../lib/helpers.js";
-import { configuration } from "../lib/graphAssets.js";
+import { navBarQhawax } from '../lib/navBarQhawax.js';
+import { chartView } from '../html/graphics.js';
+import { oneParameterRequest, requestGraphicsData, noParametersRequest } from '../requests/get.js';
+import { socket } from '../index.js';
+import { addZero } from '../lib/helpers.js';
+import { configuration } from '../lib/graphAssets.js';
 
 const SENSORS = [
   "temperature",
@@ -57,110 +53,97 @@ const createYAxisObject = () => ({
 
 const dateFormat = (timestamp) => {
   const date = new Date(Date.parse(timestamp));
-  return `${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(
-    date.getSeconds()
-  )}`;
+  return `${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}`;
 };
 
 const createSelectQhawaxOption = (name, comercial_name) => {
-  const option = document.createElement("option");
-  option.setAttribute("value", name);
-  option.innerText = `${name}: ${comercial_name}`;
+  const option = document.createElement('option');
+	option.setAttribute('value', name);
+	option.innerText = `${name}: ${comercial_name}`;
   return option;
 };
 
-const requestOptions = async (element) => {
-  const addOptions = element.querySelector("#selectQhawax");
-  await noParametersRequest("AllQhawaxInMap/").then((q) =>
-    q.forEach(async (qhawax) => {
-      await oneParameterRequest("qhawax_status/?name=", qhawax.name).then(
-        (s) =>
-          s === "ON" &&
-          addOptions.appendChild(
-            createSelectQhawaxOption(qhawax.name, qhawax.comercial_name)
-          )
-      );
-    })
-  );
+const requestOptions = async(element) => {
+  const addOptions = element.querySelector('#selectQhawax');
+  await noParametersRequest('AllQhawaxInMap/')
+  .then( q=> q.forEach(async(qhawax) => {
+    await oneParameterRequest('qhawax_status/?name=',qhawax.name)
+    .then(s =>
+      s === 'ON' && addOptions.appendChild(
+        createSelectQhawaxOption(qhawax.name, qhawax.comercial_name)))
+  }));
 };
 
 const createChart = (charts, id) => {
-  const chart = document.createElement("div");
-  const br = document.createElement("br");
-  chart.setAttribute("id", id);
-  chart.setAttribute("class", "container");
+  const chart = document.createElement('div');
+	const br = document.createElement('br');
+	chart.setAttribute('id', id);
+	chart.setAttribute('class', 'container');
   charts.appendChild(chart);
-  charts.appendChild(br);
-};
+	charts.appendChild(br);
+}
 
-const createTraces = async (time, qhawax) => {
-  const charts = document.getElementById("charts");
-  charts.innerHTML = "";
+ const createTraces = async (time, qhawax) => {
+	const charts = document.getElementById('charts');
+  charts.innerHTML = '';
 
-  let yAxis = createYAxisObject();
-  let x = [];
-  await requestGraphicsData(qhawax, time).then((g) =>
-    g.forEach((d) => {
+	let yAxis = createYAxisObject();
+	let x = [];
+	await requestGraphicsData(qhawax, time)
+    .then(g => g.forEach(d => {
       x.push(dateFormat(d.timestamp_zone));
       Object.entries(yAxis).forEach(([key, value]) => {
         yAxis[key].value.push(d[key]);
       });
-    })
-  );
+	}));
 
-  Object.entries(yAxis).forEach(([key, value]) => {
+	Object.entries(yAxis).forEach(([key, value]) => {
     createChart(charts, key);
     const trace = {
       y: yAxis[key].value,
-      x: x,
-      name: yAxis[key].name,
-      type: "scatter",
-    };
+			x: x,
+			name: yAxis[key].name,
+			type: 'scatter',
+		};
     const chart = document.getElementById(key);
     const layout = { title: yAxis[key].name, showlegend: false };
-    Plotly.newPlot(chart, [trace], layout, configuration);
-  });
+		Plotly.newPlot(chart, [trace], layout, configuration);
+	});
 };
 
 const viewGraphics = () => {
-  const graphElem = document.createElement("div");
-  graphElem.setAttribute("class", "container");
+	const graphElem = document.createElement('div');
+	graphElem.setAttribute('class', 'container');
   navBarQhawax(graphElem, chartView);
 
-  const selection = graphElem.querySelectorAll("select");
-  M.FormSelect.init(selection);
+	const selection = graphElem.querySelectorAll('select');
+	M.FormSelect.init(selection);
 
-  requestOptions(graphElem);
+	requestOptions(graphElem);
 
-  let selectedQhawax = "",
-    selectedTime = Number;
-  selection[0].onchange = () => {
-    selectedQhawax = selection[0].value.toString();
-  };
-  selection[1].onchange = () => {
-    selectedTime = selection[1].value;
-  };
+	let selectedQhawax = '', selectedTime = Number;
+	selection[0].onchange = () => {	selectedQhawax = selection[0].value.toString(); };
+	selection[1].onchange = () => {	selectedTime = selection[1].value; };
+	
+  const graphBtn = graphElem.querySelector('#graphicBtn');
+	graphBtn.addEventListener('click', () => {
+		createTraces(selectedTime, selectedQhawax);
+		socket.on(`${selectedQhawax}_processed`, res => {
+			if (res.ID === selectedQhawax) {
+				SENSORS.forEach(s => {
+					const chart = document.getElementById(s);
+					if (chart) {
+						Plotly.extendTraces(
+							chart,
+							{ y: [[res[s]]], x: [[dateFormat(res.timestamp_zone)]] },
+              [0]);
+					}
+				});
+			}
+		});
+	});
 
-  const graphBtn = graphElem.querySelector("#graphicBtn");
-  graphBtn.addEventListener("click", () => {
-    createTraces(selectedTime, selectedQhawax);
-    socket.on(`${selectedQhawax}_processed`, (res) => {
-      if (res.ID === selectedQhawax) {
-        SENSORS.forEach((s) => {
-          const chart = document.getElementById(s);
-          if (chart) {
-            Plotly.extendTraces(
-              chart,
-              { y: [[res[s]]], x: [[dateFormat(res.timestamp_zone)]] },
-              [0]
-            );
-          }
-        });
-      }
-    });
-  });
-
-  return graphElem;
+	return graphElem;
 };
 
-export { viewGraphics, dateFormat, requestOptions, createTraces };
+export { viewGraphics, dateFormat, requestOptions, createTraces};
